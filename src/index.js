8747299f1da4cb77bd1c4268d42872d14ea888de
Enmanuel jarquin <enmisac@gmail.com>
2020-02-17 01:12:24 -0600

    se re exporto los compontentes y se subio el num de version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // @flow
 
-export { default as Button } from "./Button";
 export { default as theme } from "./theme";
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
+export { default as Label } from "./Label";
+export { default as Loader } from "./Loader";
+export { default as FormText } from "./FormText";
+export { default as PasswordField } from "./PasswordField";
